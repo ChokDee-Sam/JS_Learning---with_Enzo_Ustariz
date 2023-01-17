87eb47e5e00122bb655e2326971325dff63ec679
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-01-17 21:55:16 +0700

    Commit 1 : Exercice 1 [Variables, Types]

---
+++ exercice_1/script.js
@@ -0,0 +1,35 @@
+//------------ EXERCICE 1 : Les variables, commentaires & types  ------------//
+
+// Écrire un commentaire sur plusieurs ligne
+/*
+1
+2
+3
+*/
+
+// Déclarez une constante "data" qui contient le nombre 3
+
+const data = 3;
+
+// Écrivez des données correspondant à ces différents types
+
+// String (texte)
+let str = "Hey";
+
+// Number (nombres)
+let nb = 1;
+
+// Object (object)
+let myObj = {
+    name: 'Sam',
+    age: 39,
+};
+
+// Boolean (booléen)
+let bool = true;
+
+// Déclarez trois variables sans valeur(undefined) en une ligne.
+
+let un, deux, trois;
+
+console.log(un);
