8796c5dc629b345cdb7ebf4f89fc1da7f5945910
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-03-01 23:32:56 +0600

    Commit 7 : Exercice 6 [Les Evenements]

---
+++ exercice_6/script.js
@@ -0,0 +1,21 @@
+// EXERCICE 6 : Les évènements.
+
+const cercle = document.querySelector(".cercle");
+const carre = document.querySelector(".carre");
+
+// 1. Changez la couleur du cercle en "crimson" quand votre souris le survole.
+cercle.addEventListener("mouseover", function () {
+    cercle.style.backgroundColor = "crimson";
+});
+
+// 2. Changez la couleur du carré en "violet" quand vous cliquez dessus.
+carre.addEventListener("click", function () {
+    carre.style.backgroundColor = "purple";
+});
+
+// 3. Rajoutez l'évenement "click" au document, puis loggez la position(x,y) des clics que vous effectuez sur le document.
+document.addEventListener("click", function (e) {
+    console.log(
+        `Voici les coordonnées : ${e.x}, ${e.y}, ${e.clientX}, ${e.clientY}`
+    );
+});
