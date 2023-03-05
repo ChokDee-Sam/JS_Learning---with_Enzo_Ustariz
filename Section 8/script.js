07ead04957ed42e0c3d9625b620ace711b7c0833
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-03-05 23:02:07 +0700

    Commit 11 : Section 8 [Les Fonctions CallBack]

--- Section 8/script.js
+++ Section 8/script.js
@@ -10,3 +10,32 @@ let x = 2;
 const add1 = (a, b) => a + b; // Fonction pure
 // const add2 = y => x += y;        // Pas pure du tout !!
 
+// -----------------------------------------
+// ----------- FONCTION CALLBACK -----------
+// -----------------------------------------
+
+const tab = [1, 2, 3];
+
+// ----- 1ère manière de faire -----
+// ---------------------------------
+
+tab.forEach((item) => { //method associée aux tableaux
+    console.log(item);  //item nomme chaque element de tableau
+});
+// ---------------------------------
+// ---------------------------------
+
+// ----- 2ème manière de faire -----
+// ---------------------------------
+
+function customForEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i]);
+    }
+}
+
+customForEach(tab, (item) => {
+    console.log(item);
+});
+// ---------------------------------
+// ---------------------------------
