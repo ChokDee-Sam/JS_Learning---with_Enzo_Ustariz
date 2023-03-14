b8ec242dcaee12f87741a8bce85c354641d1b4a4
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-03-14 17:00:08 +0700

    Commit 24 : Section 14 - La Programmation Orientée Objet [Les Sous-Classes]

---
+++ Section 14/6. sous classes.js
@@ -0,0 +1,56 @@
+// --------------------------------------
+// --------------------------------------
+// ------------ SOUS CLASSES ------------
+// --------------------------------------
+// --------------------------------------
+
+// Permet d'hériter de tous les paramètres
+// et d'étendre
+
+// --------------------------------------
+// Création d'une Classe initiale
+// --------------------------------------
+
+class Voiture {
+    constructor(marque, cylindrée, couleur, année, option) {
+        this.marque = marque;
+        this.cylindrée = cylindrée;
+        this.couleur = couleur;
+        this.année = année;
+        this.option = option;
+    }
+}
+
+// --------------------------------------
+// Création d'une Sous-Classe qui permet une extension
+// --------------------------------------
+
+class Moto extends Voiture {
+    // Une nouvelle Méthode dans cette Sous-Classe
+    welcome() {
+        console.log("Bienvenue dans le monde du 2 roues !");
+    }
+}
+
+// --------------------------------------
+// Première Variable : une extension vide, mais avec des propriétés héritées
+// --------------------------------------
+
+const motoBike1 = new Moto();
+console.log(motoBike1);
+
+// --------------------------------------
+// Deuxième Variable : une extension, avec des valeurs définis dans les propriétés héritées
+// --------------------------------------
+
+const motoBike2 = new Moto("Yamaha", 1300, "noire", undefined);
+console.log(motoBike2);
+
+// --------------------------------------
+// Deuxième Variable : Appel de Méthode issue de la Sous-Classe
+// --------------------------------------
+
+motoBike2.welcome();
+
+// --------------------------------------
+// --------------------------------------
