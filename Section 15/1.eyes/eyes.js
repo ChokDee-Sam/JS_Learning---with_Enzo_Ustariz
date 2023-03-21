02165015a24cb9afee747c656156a3d4fcd0773b
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-03-20 20:19:57 -1000

    Commit 27 : Section 15 - Mini-Projet 1 [Eyes Move]

---
+++ Section 15/1.eyes/eyes.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+const balls = document.querySelectorAll(".ball");
+const eye = document.querySelectorAll(".eye");
+
+container.addEventListener("mousemove", (e) => {
+    // On va créer un pourcentage selon la largeur
+    // valeur de départ *100 / valeur totale
+    const x = `${(e.clientX * 100) / window.innerWidth}%`;
+    const y = `${(e.clientY * 100) / window.innerHeight}%`;
+
+    for (let i = 0; i < 2; i++) {
+        balls[i].style.left = x;
+        balls[i].style.top = y;
+        balls[i].style.transform = `translate(-${x}, -${y})`;
+    }
+});
