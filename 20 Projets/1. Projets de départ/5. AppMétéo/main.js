cb8789316a4f1d475b4a679a41d2001f0d8dc48c
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-03-28 20:14:42 +0700

    Commit 52 - Projet JS 5 : Application Météo [JS : Afficher les données]

--- 20 Projets/1. Projets de départ/5. AppMétéo/main.js
+++ 20 Projets/1. Projets de départ/5. AppMétéo/main.js
@@ -3,15 +3,12 @@
 
 const loader = document.querySelector(".loader-container");
 const errorInformation = document.querySelector(".error-information");
-
-// const cityName = document.querySelector(".city-name");
-// const countryName = document.querySelector(".country-name");
 // const infoIconContainer = document.querySelector(".info-icon-container");
 
 // ----------------------------------------------------------
 // On récupère des données de manière asynchrone, dans une API
 // On transforme ces données au format JSON
-// Puis on crée un Objet avec uniquement les données qui nous intéressent
+// Puis, on crée un Objet avec uniquement les données qui nous intéressent
 // ----------------------------------------------------------
 
 async function getWeatherData() {
@@ -19,7 +16,7 @@ async function getWeatherData() {
         const response = await fetch(
             "http://api.airvisual.com/v2/nearest_city?key=5f684ad9-440e-4b8c-90dd-1e2e5f8327d5"
         );
-        console.log(response);
+        // console.log(response);
 
         const responseData = await response.json();
         // console.log(responseData);
@@ -31,7 +28,25 @@ async function getWeatherData() {
             temperature: responseData.data.current.weather.tp,
         };
         // console.log(sortedData);
+
+        populateUI(sortedData);
     } catch (error) {}
 }
 
 getWeatherData();
+
+// ----------------------------------------------------------
+
+const cityName = document.querySelector(".city-name");
+const countryName = document.querySelector(".country-name");
+const temperature = document.querySelector(".temperature");
+const infoIcon = document.querySelector(".info-icon");
+
+function populateUI(data) {
+    cityName.textContent = data.city;
+    countryName.textContent = data.country;
+    temperature.textContent = `${data.temperature}°`;
+    infoIcon.src = `ressources/icons/${data.iconId}.svg`;
+    infoIcon.style.width = "150px";
+    loader.classList.remove("active");
+}
